3dd7603037a8132ecc1379a3e062e2d92328593a
ancaicedo <77050076+ancaicedo@users.noreply.github.com>
2022-12-01 00:44:49 -0700

    added realistic gravity

--- Assets/OpenHaptics/HapticDemos/DemoScripts/BlockPuzzleController.cs
+++ Assets/OpenHaptics/HapticDemos/DemoScripts/BlockPuzzleController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,11 +8,13 @@ public class BlockPuzzleController : MonoBehaviour {
 	public GameObject[] Balls = {null, null};
 	private Vector3[] ToyPosition;
 	private Quaternion[] ToyRotation;
-
+	private Rigidbody ball;
+	public float gravityMultiplier = 2f;
 
 	// Remember the original positions of the blocks.
 	void Start () 
 	{
+		
 		ToyPosition = new Vector3[Balls.Length];
 		ToyRotation = new Quaternion[Balls.Length];
 		for (int ii = 0; ii < Balls.Length; ii++)
@@ -19,6 +22,8 @@ public class BlockPuzzleController : MonoBehaviour {
 			ToyPosition [ii] = Balls [ii].transform.position;
 			ToyRotation [ii] = Balls [ii].transform.rotation;
 		}
+
+		ball = Balls[0].GetComponent<Rigidbody>();
 	}
 
 	// Return the blocks to their original position.
@@ -35,6 +40,17 @@ public class BlockPuzzleController : MonoBehaviour {
 		}
 	}
 
+	void IncreaseWeight()
+	{
+		Debug.Log("Increase the mass of the first ball");
+		ball.mass += 1;
+	}
+
+	void DecreaseWeight()
+	{
+		Debug.Log("Decrease the mass of the first ball");
+		ball.mass -= 1;
+	}
 
 	// Update is called once per frame
 	void Update () 
@@ -50,5 +66,10 @@ public class BlockPuzzleController : MonoBehaviour {
         {
             Application.Quit();
         }
-    }
+	}
+
+	void FixedUpdate()
+	{
+		ball.AddForce(Physics.gravity * gravityMultiplier, ForceMode.Acceleration);
+	}
 }
